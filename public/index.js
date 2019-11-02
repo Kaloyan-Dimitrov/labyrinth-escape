f63a6fa7ab64ea41021424714f24c7d757ee1441
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2019-11-02 18:27:38 +0200

    Refurbished animations, changed some colors and small functionallity changes

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 const SVG = require('svg.js');
 const $ = require('jquery');
 const bootstrap = require('bootstrap');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 $(() => {
     $('.btn').button();
     let mouseDown = 0;
@@ -13,9 +17,9 @@ $(() => {
     const cellW = 20;
     const cellH = 20;
     const gridWidth = window.innerWidth / cellW;
-    const gridHeight = (window.innerHeight * 0.8) / cellH;
+    const gridHeight = (window.innerHeight * 0.85) / cellH;
     const canvasWidth = window.innerWidth;
-    const canvasHeight = window.innerHeight * 0.8;
+    const canvasHeight = window.innerHeight * 0.85;
     const draw = SVG('drawing').size(canvasWidth, canvasHeight + 4);
     let startX, startY, endX, endY;
     grid = [];
@@ -27,7 +31,7 @@ $(() => {
                 opacity: 0.3
             });
             grid[i][j].mouseover(function () {
-                if (mouseDown && $(grid[i][j].node).attr('fill') == '#ffffff' && $('#placing').children('.active').children()[0].id == 'wall') {
+                if (!(i == startX && j == startY) && !(i == endX && j == endY) && mouseDown && $(grid[i][j].node).attr('fill') == '#ffffff' && $('#placing').children('.active').children()[0].id == 'wall') {
                     const toAnimate = draw.rect(cellW, cellH).move(this.x(), this.y()).fill('#fff').stroke({
                         color: '#000',
                         opacity: 0.3
@@ -48,9 +52,30 @@ $(() => {
                     }, 300);
                     this.fill('#000');
                 }
+                if (mouseDown && !(i == startX && j == startY) && !(i == endX && j == endY) && $(grid[i][j].node).attr('fill') == '#000000' && $('#placing').children('.active').children()[0].id == 'empty') {
+                    const toAnimate = draw.rect(cellW, cellH).move(this.x(), this.y()).fill('#000').stroke({
+                        color: '#fff',
+                        opacity: 0.3
+                    })
+                    toAnimate.animate({
+                        duration: 100
+                    }).fill({
+                        color: '#fff'
+                    });
+                    toAnimate.animate({
+                        duration: 100
+                    }).scale(1.5, 1.5).animate({
+                        when: 'after',
+                        duration: 100
+                    }).scale(1, 1);
+                    setTimeout(() => {
+                        toAnimate.remove();
+                    }, 300);
+                    this.fill('#fff');
+                }
             });
             grid[i][j].click(function () {
-                if ($(grid[i][j].node).attr('fill') == '#ffffff' && $('#placing').children('.active').children()[0].id == 'wall') {
+                if (!(i == startX && j == startY) && !(i == endX && j == endY) && $(grid[i][j].node).attr('fill') == '#ffffff' && $('#placing').children('.active').children()[0].id == 'wall') {
                     const toAnimate = draw.rect(cellW, cellH).move(this.x(), this.y()).fill('#fff').stroke({
                         color: '#000',
                         opacity: 0.3
@@ -89,6 +114,27 @@ $(() => {
                         endY
                     })
                 }
+                if (!(i == startX && j == startY) && !(i == endX && j == endY) && $(grid[i][j].node).attr('fill') == '#000000' && $('#placing').children('.active').children()[0].id == 'empty') {
+                    const toAnimate = draw.rect(cellW, cellH).move(this.x(), this.y()).fill('#000').stroke({
+                        color: '#fff',
+                        opacity: 0.3
+                    })
+                    toAnimate.animate({
+                        duration: 100
+                    }).fill({
+                        color: '#fff'
+                    });
+                    toAnimate.animate({
+                        duration: 100
+                    }).scale(1.5, 1.5).animate({
+                        when: 'after',
+                        duration: 100
+                    }).scale(1, 1);
+                    setTimeout(() => {
+                        toAnimate.remove();
+                    }, 300);
+                    this.fill('#fff');
+                }
             });
         }
     }
@@ -115,9 +161,29 @@ $(() => {
                 cell.visited = false;
                 cell.parent = null;
             });
-        })
+        });
     });
-    $('#solve').click(() => {
+    let finished = false;
+    $('#stop').click(() => {
+        finished = true;
+        grid.forEach(row => {
+            row.forEach(cell => {
+                if ($(cell.node).attr('fill') != '#000000') cell.fill('#ffffff');
+                cell.visited = false;
+                cell.parent = null;
+            });
+        });
+        $('.offLabel').removeClass('disabled');
+        $('.off').prop('disabled', false);
+        $('#solve').css('display', 'inline-block');
+        $('#stop').css('display', 'none');
+    })
+    $('#solve').click(async () => {
+        $('.offLabel').addClass('disabled');
+        $('.off').attr('disabled', 'true');
+        $('#solve').css('display', 'none');
+        $('#stop').css('display', 'inline-block');
+        finished = false;
         let queue = [];
         queue.push({
             x: startX,
@@ -127,7 +193,7 @@ $(() => {
         let dy = [0, -1, 0, 1];
         while (queue.length > 0) {
             const parent = queue.shift();
-            let finished = false;
+            // grid[parent.x][parent.y].visited = true;
             for (let i = 0; i < 4; i++) {
                 const current = {
                     x: parent.x + dx[i],
@@ -138,18 +204,29 @@ $(() => {
                 if (grid[current.x][current.y].visited) continue;
                 grid[current.x][current.y].visited = true;
                 grid[current.x][current.y].parentCoords = parent;
-                grid[current.x][current.y].fill('#070');
-                console.log(current.x, current.y);
+                grid[current.x][current.y].animate({
+                    duration: 300,
+                    swing: true
+                }).fill('#d051e0');
                 queue.push(current);
             }
+            await sleep(5);
+            grid[parent.x][parent.y].animate({
+                duration: 300,
+                swing: true
+            }).fill('#36cae0');
             if (finished) break;
         }
-        let current = grid[endX][endY];
-        while (current != grid[startX][startY]) {
-            current.fill('#a0a');
-            console.log(current.parentCoords)
-            current = grid[current.parentCoords.x][current.parentCoords.y];
+        // await sleep(1000);
+        if (finished) {
+            let current = grid[endX][endY];
+            while (current != grid[startX][startY]) {
+                current.animate(1000, 0, 'last').fill('#fff654');
+                current = grid[current.parentCoords.x][current.parentCoords.y];
+            }
+            current.animate(1000, 0, 'last').fill('#fff654');
         }
-        current.fill('#a0a');
+        await sleep(2000);
+        $('#stop').prop('disabled', false);
     });
 });
